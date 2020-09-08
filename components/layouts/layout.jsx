6dd6a846b1553efd31af35db74a8d33faf8ce18e
Jucian0 <juciano@outlook.com.br>
2020-09-08 20:02:20 -0300

    add improviments layout

--- components/layouts/layout.jsx
+++ components/layouts/layout.jsx
@@ -5,68 +5,64 @@ import styled from 'styled-components'
 import Navbar from '../navbar'
 import Sidebar from '../sidebar'
 
-
 const Main = styled.main`
-  display:flex;
-  flex:1;
-  flex-direction:column;
-  width:100%;
-  height:100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
 `
 
 const Container = styled.div`
-  display:flex;
-  flex-direction:row;
-  width:100%;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
 `
 const MainContent = styled.div`
-  display:flex;
-  width: ${({open})=>!open? "100%" :"calc(100% - 200px)"};
-  position:fixed;
-  height:100%;
-  justify-content:center;
-  float:left;
-  left:${({open})=> open? "200px":"0"};
+  display: flex;
+  width: ${({ open }) => (!open ? '100%' : 'calc(100% - 200px)')};
+  height: calc(100% - 60px);
+  position: fixed;
+  justify-content: center;
+  float: left;
+  left: ${({ open }) => (open ? '200px' : '0')};
   transition: all 0.5s ease-in-out;
-  overflow:auto;
+  overflow: auto;
 `
 
 const Content = styled.div`
-  display:block;
-  width:100%;
-  height:100%;
-  padding:40px;
-  max-width:1440px;
-  background-color:${({theme})=> theme.colors.bkgContent};
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 40px;
+  max-width: 1440px;
+  background-color: ${({ theme }) => theme.colors.bkgContent};
 `
 
 // eslint-disable-next-line react/prop-types
 export default function Page({ children }) {
-
   const [open, setOpen] = useState(true)
 
-  const resize = ()=>{
-    if(window?.innerWidth > 989){
+  const resize = () => {
+    if (window?.innerWidth > 989) {
       setOpen(true)
-    }else if (window?.innerWidth <= 989){
+    } else if (window?.innerWidth <= 989) {
       setOpen(false)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener('resize', resize)
     resize()
-  },[])
+  }, [])
 
   return (
     <Main>
-      <Navbar setOpen={()=>setOpen(!open)}/>
+      <Navbar setOpen={() => setOpen(!open)} />
       <Container>
-        <Sidebar open={open}/>
+        <Sidebar open={open} />
         <MainContent open={open}>
-          <Content>
-            {children}
-          </Content>
+          <Content>{children}</Content>
         </MainContent>
       </Container>
     </Main>
