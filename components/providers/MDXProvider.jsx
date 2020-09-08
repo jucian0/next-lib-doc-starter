5454b98ab296512f45dcaa7cc7a98fe69b5dcfcc
Jucian0 <juciano@outlook.com.br>
2020-09-08 20:02:38 -0300

    add improviments providers

--- components/providers/MDXProvider.jsx
+++ components/providers/MDXProvider.jsx
@@ -1,18 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import CodeBlock from "../codeblock"
-import { H1, H2, P,Pre,H3,H4 } from '../md'
-
+import CodeBlock from '../codeblock'
+import { H1, H2, P, Pre, H3, H4 } from '../md'
 
 const mdComponents = {
-  code:CodeBlock,
+  code: CodeBlock,
   h1: H1,
-  h2:H2,
-  h3:H3,
-  h4:H4,
-  p:P,
-  Inline:Pre
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  p: P,
+  Inline: Pre,
 }
 
 export default ({ children }) => (
