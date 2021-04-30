89694a62f5484192270e3d42e360b8c4d7b80c5a
jucian0 <juciano@outlook.com.br>
2021-04-30 11:28:48 -0300

    adding sidebar

--- components/sidebar/index.jsx
+++ components/sidebar/index.jsx
@@ -1,47 +1,73 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useMemo,useState } from 'react';
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import { GoChevronRight } from "react-icons/go";
 import { ThemeContext } from 'styled-components';
 
-import { Container, Item, SubList,Item2 } from './styles';
+import { Aside, Item, Items, SubItems, SubItem } from './styles';
 
 
 const links = [
   {
-    link:'input',
-    name:'Input',
-    sublink:[
+    label:'Introduction',
+    link:'',
+    subItems:[
       {
-        link:'input',
-        name:'Input'
+        label:'Quick Start',
+        link:'/doc/input0'
+      }
+    ]
+  },
+  {
+    label:'API',
+    link:'',
+    subItems:[
+      {
+        label:'useForm()',
+        link:'/doc/use-form'
       },
       {
-        link:'input',
-        name:'Input'
+        label:'useFormContext',
+        link:'/doc/use-form-context'
       },
       {
-        link:'input',
-        name:'Input'
+        label:'useValidation()',
+        link:'/doc/use-validation'
+      },
+      {
+        label:'<ContextForm/>',
+        link:'/doc/context-form'
       },
     ]
   },
   {
-    link:'input',
-    name:'Input'
-  },
-  {
-    link:'input',
-    name:'Input'
-  },
-  {
-    link:'input',
-    name:'Input'
-  },
-  {
-    link:'input',
-    name:'Input'
+    label:'Examples',
+    link:'',
+    subItems:[
+      {
+        label:'Controlled form',
+        link:'/doc/controlled-form'
+      },
+      {
+        label:'Debounced form',
+        link:'/doc/debounced-form'
+      },
+      {
+        label:'Uncontrolled form',
+        link:'/doc/uncontrolled-form'
+      },
+      {
+        label:'Validation',
+        link:'/doc/validation-form'
+      },
+      {
+        label:'Custom fields',
+        link:'/doc/custom-fields'
+      },
+    ]
   },
 ]
 
@@ -53,40 +79,42 @@ function ActiveLink({ href,children }){
   const isActive = useMemo(()=> router.pathname === href,[router.pathname, href])
 
   return (
-    <Item className={isActive && "active"}>
+    <SubItem className={isActive && "active"}>
       <GoChevronRight color={isActive? theme.colors.primary:'transparent'} />
       <Link href={href} className={isActive && "active"}>{children}</Link>
-    </Item>
+    </SubItem>
   ) 
 }
 
 
 function Sidebar({open}) {
 
+  const [expanded,setExpanded] = useState('')
+
   return (
-    <Container open={open}>
-      {
-        links.map(link=>(
-          <Item2 key={link.name}>
-            <SubList>
+    <Aside open={open}>
+      <Items>
+        {
+          links.map(link =>(
+            <Item>
+              <span onClick={()=>setExpanded('')}>
+                {link.label}
+              </span>
               {
-            link.name
+            expanded === ''
           }
-              {
-            link.sublink?.map(sub=>(
-              <ActiveLink key={sub.name} href={sub.link}>
+              <SubItems>
                 {
-            sub.name
-          }
-              </ActiveLink>
+            link.subItems?.map(subItem =>(
+              <ActiveLink href="/">{subItem.label}</ActiveLink>
             ))
           }
-            </SubList>
-          </Item2>
-        ))
-      }
-
-    </Container>
+              </SubItems>
+            </Item>
+          ))
+        }
+      </Items>
+    </Aside>
   );
 }
 
--- components/sidebar/styles.js
+++ components/sidebar/styles.js
@@ -1,21 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components'
 
-export const SubList = styled.div`
-  position: fixed;
-  display: flex;
-  flex-shrink: 0;
-  flex-direction: column;
-  padding-top: 20px;
-  position: sticky;
-  overflow:auto;
-  top:60px;
-  transition: margin-left 0.5s ease-in-out;
-  width: 240px;
-  color: ${({ theme }) => theme.colors.textColor};
-`
-
-export const Container = styled.aside`
+export const Aside = styled.aside`
   position: fixed;
   display: flex;
   height: calc(100vh - 60px);
@@ -23,14 +9,48 @@ export const Container = styled.aside`
   background-color: ${({ theme }) => theme.colors.white};
   border-right: 1px solid ${({ theme }) => theme.colors.border};
   flex-direction: column;
-  padding-top: 20px;
+  padding: 20px;
   position: sticky;
   overflow:auto;
   top:60px;
   transition: margin-left 0.5s ease-in-out;
   margin-left: ${({ open }) => (open ? 0 : '-240px')};
-  width: 240px;
+  width: 260px;
   color: ${({ theme }) => theme.colors.textColor};
+
+  ul{
+    padding-left:0px;
+  }
+  li{
+    margin:15px;
+  }
+`
+
+export const Items = styled.ul`
+
+`
+
+export const Item = styled.li`
+  list-style:none;
+  cursor:pointer;
+  & span{
+    font-weight:600;
+    font-size:1.2em;
+  }
+`
+
+export const SubItems = styled.ul`
+ /* padding-top:20px;
+ padding-left:10px;
+ padding-bottom:0; */
+  /* position: relative;
+  display: flex;
+  flex-shrink: 0;
+  flex-direction: column;
+  padding-top: 20px;
+  overflow:auto;
+  transition: margin-left 0.5s ease-in-out;
+  color: ${({ theme }) => theme.colors.textColor}; */
 `
 
 export const Indicator = styled.div`
@@ -41,50 +61,103 @@ export const Indicator = styled.div`
   margin: 3px;
 `
 
-export const Item2 = styled.div`
+export const SubItem = styled.li`
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  padding: 15px 30px;
-  font-weight:600;
-  &:hover{
-    cursor:pointer;
-  }
-`
+  font-weight: 500;
 
-export const Item = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  padding: 15px 30px;
+  &:hover{
+     cursor:pointer;
+  }
 
   & a {
     display: flex;
     cursor: pointer;
     font-size: 16px;
-    font-weight: 400;
     text-decoration: none;
     color: ${({ theme }) => theme.colors.textColor};
   }
-
+   
   &.active {
     background-color: ${({ theme }) => theme.colors.border};
     a {
       color: ${({ theme }) => theme.colors.primary};
-      font-weight:600;
     }
     ${Indicator} {
       background-color: ${({ theme }) => theme.colors.primary};
     }
   }
-
+   
   &:hover {
     ${Indicator} {
       background-color: ${({ theme }) => theme.colors.primary};
     }
     a {
-      font-weight:600;
       color: ${({ theme }) => theme.colors.primary};
     }
   }
+
 `
+
+// export const SubList = styled.div`
+//   position: fixed;
+//   display: flex;
+//   flex-shrink: 0;
+//   flex-direction: column;
+//   padding-top: 20px;
+//   position: sticky;
+//   overflow:auto;
+//   top:60px;
+//   transition: margin-left 0.5s ease-in-out;
+//   color: ${({ theme }) => theme.colors.textColor};
+// `
+
+
+// export const Item2 = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: flex-start;
+//   padding: 15px 30px;
+//   font-weight:600;
+//   &:hover{
+//     cursor:pointer;
+//   }
+// `
+
+// export const Item = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: flex-start;
+//   padding: 15px 30px;
+
+//   & a {
+//     display: flex;
+//     cursor: pointer;
+//     font-size: 16px;
+//     font-weight: 400;
+//     text-decoration: none;
+//     color: ${({ theme }) => theme.colors.textColor};
+//   }
+
+//   &.active {
+//     background-color: ${({ theme }) => theme.colors.border};
+//     a {
+//       color: ${({ theme }) => theme.colors.primary};
+//       font-weight:600;
+//     }
+//     ${Indicator} {
+//       background-color: ${({ theme }) => theme.colors.primary};
+//     }
+//   }
+
+//   &:hover {
+//     ${Indicator} {
+//       background-color: ${({ theme }) => theme.colors.primary};
+//     }
+//     a {
+//       font-weight:600;
+//       color: ${({ theme }) => theme.colors.primary};
+//     }
+//   }
+// `
