c82b3a3818ec96c5791b296add0d9b123ce3beb3
jucian0 <juciano@outlook.com.br>
2021-04-29 22:02:25 -0300

    improving footer

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -29,18 +29,17 @@ export default function Footer(){
            </Logo>
          </FooterLogo>
          <FooterInformation>
-           <h1>Social</h1>
-           <Link href="/">Let's start</Link>
-           <Link href="/">Github</Link>
-           <Link href="/">Twitter</Link>
-           <Link href="/">Version</Link>
+           <h1>Docs</h1>
+           <Link href="/">Getting stated</Link>
+           <Link href="/">API</Link>
+           <Link href="/">Docs</Link>
          </FooterInformation>
          <FooterInformation>
            <h1>Community</h1>
-           <Link href="/">Let's start</Link>
            <Link href="/">Github</Link>
-           <Link href="/">Twitter</Link>
-           <Link href="/">Version</Link>
+           <Link href="/">Request a feature</Link>
+           <Link href="/">Report a bug</Link>
+           <Link href="/">Leave a start</Link>
          </FooterInformation>
        </FooterContent>
      </Container>
